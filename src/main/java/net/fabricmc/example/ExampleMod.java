10cf114b1765b81ecd7a50b3d8930cd42296970a
PradyunS <pradyun.settI@gmail.com>
2020-06-10 19:34:26 +1200

    can now initialise item but not entity

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -2,17 +2,17 @@ package net.fabricmc.example;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
-//import java.rmi.registry.Registry;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.registry.Registry;
 
 public class ExampleMod implements ModInitializer {
 	public static final Item test = new Item(new Item.Settings().group(ItemGroup.COMBAT));
-
+	
 	@Override
 	public void onInitialize() {
 		Registry.register(
 			Registry.ITEM,
-			new Identifier("tutorial", "fabric_item"), test);
+			new Identifier("gamer", "test"), test
+			);
 	} 
 }
--- src/main/java/net/fabricmc/example/WaterManEntity.java
+++ src/main/java/net/fabricmc/example/WaterManEntity.java
@@ -1,31 +1,36 @@
 package net.fabricmc.example;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.mob.EndermanEntity;
-import net.minecraft.util.Identifier;
 
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraft.entity.EntityType;
-import net.minecraft.*;
-import net.minecraft.entity.EntityCategory;
-import net.minecraft.entity.*;
-import net.*;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+import net.minecraft.util.Identifier;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityCategory;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.world.World;
 
 
 public class WaterManEntity extends EndermanEntity {
 
     WaterManEntity() {
-        //super();
-        super(null, null);
+        super(EntityType.ENDERMAN, null);
     }
 
-    /*
-    public static final EntityType<WaterManEntity> WATER_MAN =
-    Registry.register(
+    
+    public static final EntityType<?> WATER_MAN =
+     Registry.register(
         Registry.ENTITY_TYPE,
-        new Identifier("wiki_entity", "waterman"),
-        FabricEntityTypeBuilder.create(EntityCategory.MONSTER,
-        (EntityType.EntityFactory<WaterManEntity>) WaterManEntity::new)
-    );
-    */
+        new Identifier("gamer", "WATER_MAN"),
+        FabricEntityTypeBuilder.create(EntityCategory.MONSTER, (EntityType.EntityFactory<WaterManEntity>) WaterManEntity::new).dimensions(EntityDimensions.fixed(1, 3)).build()
+    ); 
+
+    public static Item registerEntitySpawnEgg(EntityType<?> type, int c1, int c2) {
+        SpawnEggItem item = new SpawnEggItem(type, c1, c2, new Item.Settings().group(ItemGroup.MISC));
+        return item;
+    }
+    
 }
