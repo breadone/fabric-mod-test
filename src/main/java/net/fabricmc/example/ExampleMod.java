20c0453b83f4b24bfddc5cdd4654647330ec6920
pradyun laptop <pradyun.setti@gmail.com>
2020-06-23 16:24:54 +1200

    23-06-2020

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -3,8 +3,12 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.mixin.object.builder.MaterialBuilderAccessor;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.item.FoodComponent;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.block.MaterialColor;
 
 public class ExampleMod implements ModInitializer {
 
@@ -12,25 +16,29 @@ public class ExampleMod implements ModInitializer {
 	public static final FoodComponent fcCheese = (new FoodComponent.Builder()).hunger(2).saturationModifier(2.4F).meat().build();
 	public static final FoodComponent fcblockCheese = (new FoodComponent.Builder()).hunger(3).saturationModifier(2.4F).meat().build();
 	
+	//public static final MaterialColor;
+	//public static final Material cblockCheese = (new Material().METAL;
+
 	public static final Item big_sticc = new Item(new Item.Settings().group(ItemGroup.COMBAT).maxDamage(20));
     public static final Item mato_soup = new Item(new Item.Settings().group(ItemGroup.FOOD).food(fcMatoSoup).maxCount(64));
 	public static final Item cheese = new Item(new Item.Settings().group(ItemGroup.FOOD).food(fcCheese));
+	//public static final Block blockCheese = new Block(new Block.Settings().of(material));
 
 	@Override
 	public void onInitialize() {
 		Registry.register(
 			Registry.ITEM,
-			new Identifier("big_sticc"), big_sticc
+			new Identifier("breadmod:big_sticc"), big_sticc
 			);
 
 		Registry.register(
 			Registry.ITEM,
-			new Identifier("mato_soup"), mato_soup
+			new Identifier("breadmod:mato_soup"), mato_soup
 			);
 
 		Registry.register(
 			Registry.ITEM,
-			new Identifier("cheese"), cheese
+			new Identifier("breadmod:cheese"), cheese
 		);
 	} 
 }
